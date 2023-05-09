d9d914044de0b10102813d3bd9025b4eb64bdc5c
AlvaroGalvezBarba <122776088+AlvaroGalvezBarba@users.noreply.github.com>
2023-05-09 11:57:49 +0200

    port 3307

--- Login/clases/Conexion.php
+++ Login/clases/Conexion.php
@@ -4,6 +4,7 @@ class Conexion
 {
     private $host;
     private $userName;
+    private $port;
     private $password;
     private $db;
     protected $conn;
@@ -25,10 +26,11 @@ class Conexion
         if (!feof($configFile)) {
             $connData = fgetcsv($configFile);
             $this->host = $connData[0];
-            $this->userName = $connData[1];
-            $this->password = $connData[2];
-            $this->db = $connData[3];
-            $this->conn = new mysqli($this->host, $this->userName, $this->password, $this->db);
+            $this->port = $connData[1];
+            $this->userName = $connData[2];
+            $this->password = $connData[3];
+            $this->db = $connData[4];
+            $this->conn = new mysqli($this->host,$this->port, $this->userName, $this->password, $this->db);
             if ($this->conn->connect_error) {
                 die("Connection failed: " . $this->conn->connect_error);
             }
