9bdf11d3beb7c59fba29d99517040a12b2d57045
LuciaAlbi <97888214+LuciaAlbi@users.noreply.github.com>
2023-05-10 11:40:14 +0200

    update product

--- Login/pages/Almacen/product.php
+++ Login/pages/Almacen/product.php
@@ -15,22 +15,48 @@ require_once "../../vendor/autoloader.php";
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <title>Buscar</title>
-    <style type='text/css'>
-        @import 'buscar.css';
-    </style>
+
 </head>
 
 
 <body>
-    <div class="cajabuscar">
-        <form method="get" id="buscarform">
-            <fieldset>
-                <input type="text" id="s" value="" placeholder="Buscar" />
-                <input class="button" type="submit" value="" />
-                <i class="search"></i>
-            </fieldset>
-        </form>
-    </div>
+<header>
+        <div class="container">
+            <div class="row">
+                <nav class="navbar navbar-expand-sm navbar-dark">
+                    <a class="navbar-brand col-1" href="header.php">
+                        <img src="../../assests/img/image-removebg-preview.png" alt="Avatar Logo" style="width:50px;">
+                    </a>
+                    <h2 class="col-9 text-center">La casa del electrodomestico</h2>
+                    <div class="dropdown col-5">
+                        <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown">
+                            Dropdown button
+                        </button>
+                        <ul class=" dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Poner nombre</a></li>
+                            <li><a class="dropdown-item" href="#">Poner nombre</a></li>
+                            <li><a class="dropdown-item" href="#">Poner nombre</a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </header>
+
+    <div class="container-fluid">
+  <form class="form-inline">
+    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+  </form>
+</div>
+<div class="container-fluid">
+  <form class="form-inline">
+    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+  </form>
+</div>
+
+
 </body>
 
 </html>
--- header/header_dropdown.php
+++ header/header_dropdown.php
@@ -20,7 +20,7 @@
             <div class="row">
                 <nav class="navbar navbar-expand-sm navbar-dark">
                     <a class="navbar-brand col-1" href="header.php">
-                        <img src="assests/images/image-removebg-preview.png" alt="Avatar Logo" style="width:50px;">
+                        <img src="assests/img/image-removebg-preview.png" alt="Avatar Logo" style="width:50px;">
                     </a>
                     <h2 class="col-9 text-center">La casa del electrodomestico</h2>
                     <div class="dropdown col-5">
