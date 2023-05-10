23e287cdff6f308e5ef4141f6b94c907efde882a
AlvaroGalvezBarba <122776088+AlvaroGalvezBarba@users.noreply.github.com>
2023-05-10 09:36:57 +0200

    Cambiamos landing

--- Login/pages/Almacen/Landing.php
+++ Login/pages/Almacen/Landing.php
@@ -12,15 +12,12 @@ require_once "../../vendor/autoloader.php";
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Landing almacen</title>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
-		integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 
 
 <body>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
-		integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
-		crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 	<header>
 		<h1>Almacenamiento de Productos</h1>
 		<table class="default">
@@ -34,7 +31,8 @@ require_once "../../vendor/autoloader.php";
 									<h2>Producto</h2>
 								</div>
 							</a>
-				</td>
+						</div>
+					</section>
 				</td>
 				<td>
 					<section>
@@ -44,14 +42,22 @@ require_once "../../vendor/autoloader.php";
 									<h2>Pedido</h2>
 								</div>
 							</a>
-
 						</div>
 					</section>
 				</td>
-
 			</tr>
-
+		</table>
 	</header>
+	<div class="container">
+		<div class="row">
+			<div class="card" style="width:400px">
+				<img class="card-img-top" src="../image.png" alt="card image">
+				<div class="card-body">
+					<h4 class="card-title text-center">PRODUCTOS</h4>
+				</div>
+			</div>
+		</div>
+	</div>
 	<footer>
 		<p>Derechos reservados &copy; 2023</p>
 	</footer>
