2b312a1ad6889a69b358c15fd5e3eb9af85be7e9
AlvaroGalvezBarba <122776088+AlvaroGalvezBarba@users.noreply.github.com>
2023-05-10 11:40:02 +0200

    update landing

--- Login/pages/Almacen/Landing.php
+++ Login/pages/Almacen/Landing.php
@@ -13,16 +13,35 @@ require_once "../../vendor/autoloader.php";
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Landing almacen</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+	<link rel="stylesheet" href="../../assets/css/style.css">
 </head>
 
 
 <body>
+	<header>
+		<div class="container-fluid">
+			<div class="row">
+				<nav class="navbar navbar-expand-sm navbar-dark">
+					<a class="navbar-brand col-1" href="header.php">
+						<img src="../../assets/img/image-removebg-preview.png" alt="Avatar Logo" style="width:50px;">
+					</a>
+					<h2 class="col-10 text-center">La casa del electrodomestico</h2>
+				</nav>
+			</div>
+		</div>
+	</header>
 	<div class="container">
 		<div class="row">
-			<div class="card" style="width:400px">
-				<img class="card-img-top" src="../../assets/img/image.png" alt="card image">
+			<div class="card offset-2 col-4" style="width:400px; margin-top:100px;">
+				<a href="product.php"><img class="card-img-top" src="../../assets/img/image.png" alt="card image"></a>
 				<div class="card-body">
 					<h4 class="card-title text-center">PRODUCTOS</h4>
+				</div>
+			</div>
+			<div class="card offset-1 col-4" style="width:400px; margin-top:100px;">
+				<a href="newOrder.php"><img class="card-img-top" src="../../assets/img/product.png" alt="card image"></a>
+				<div class="card-body">
+					<h4 class="card-title text-center">PEDIDOS</h4>
 				</div>
 			</div>
 		</div>
