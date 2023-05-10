e7b68af31e2ff695d8f6d4fb02772892407e565d
LuciaAlbi <97888214+LuciaAlbi@users.noreply.github.com>
2023-05-10 12:24:38 +0200

    footer y politica privacidad

---
+++ header/footer.php
@@ -0,0 +1,12 @@
+<footer>
+        <div class="container-fluid" style="background-color: #4D6E9B; margin-top:100px;position:fixed;">
+            <div class="row">
+				<div class="navbar navbar-fixed-bottom">
+                <div class="col-12 text-center">
+				<p>Derechos reservados &copy; 2023</p>
+                <p><a href="../../assets/privacity/privacityPolitic">Politica de privacidad</a></p>
+                </div>
+				</div>
+            </div>
+        </div>
+    </footer>
