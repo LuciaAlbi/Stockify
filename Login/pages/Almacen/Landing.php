3cfecb05f2b32807026daf3740209ddb6a23126c
AlvaroGalvezBarba <122776088+AlvaroGalvezBarba@users.noreply.github.com>
2023-05-10 12:18:32 +0200

    Update Landing.php

--- Login/pages/Almacen/Landing.php
+++ Login/pages/Almacen/Landing.php
@@ -58,11 +58,13 @@ require_once "../../vendor/autoloader.php";
 		</div>
 	</div>
 	<footer>
-        <div class="container-fluid" style="background-color: #4D6E9B; margin-top:100px">
+        <div class="container-fluid" style="background-color: #4D6E9B; margin-top:100px;position:fixed;">
             <div class="row">
+				<div class="navbar navbar-fixed-bottom">
                 <div class="col-12 text-center">
 				<p>Derechos reservados &copy; 2023</p>
                 </div>
+				</div>
             </div>
         </div>
     </footer>
