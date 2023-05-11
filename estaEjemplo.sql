6162305f5cde69ff4d5fc949d2c122fcdb59c7d8
EmHeGe <emhege@floridauniversitaria.es>
2023-05-11 12:50:23 +0200

    Create estaEjemplo.sql

---
+++ estaEjemplo.sql
@@ -0,0 +1,22 @@
+INSERT INTO esta (ref, almacen_id, id_ubi) 
+VALUES ('00001',1,1);
+INSERT INTO esta (ref, almacen_id, id_ubi) 
+VALUES ('00002',1,2);
+INSERT INTO esta (ref, almacen_id, id_ubi) 
+VALUES ('00002',1,3);
+INSERT INTO esta (ref, almacen_id, id_ubi) 
+VALUES ('00002',1,4);
+INSERT INTO esta (ref, almacen_id, id_ubi) 
+VALUES ('00002',1,5);
+INSERT INTO esta (ref, almacen_id, id_ubi) 
+VALUES ('00002',1,6);
+INSERT INTO esta (ref, almacen_id, id_ubi) 
+VALUES ('00002',1,7);
+INSERT INTO esta (ref, almacen_id, id_ubi) 
+VALUES ('00002',1,8);
+INSERT INTO esta (ref, almacen_id, id_ubi) 
+VALUES ('00002',1,9);
+INSERT INTO esta (ref, almacen_id, id_ubi) 
+VALUES ('00002',1,10);
+INSERT INTO esta (ref, almacen_id, id_ubi) 
+VALUES ('00002',1,11);
