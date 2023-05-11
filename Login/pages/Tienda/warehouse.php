37bad1fbafef20f0959d4eecd2efbab4ced0a736
LuciaAlbi <97888214+LuciaAlbi@users.noreply.github.com>
2023-05-11 08:44:15 +0200

    Update warehouse.php

--- Login/pages/Tienda/warehouse.php
+++ Login/pages/Tienda/warehouse.php
@@ -13,15 +13,15 @@ require_once "../../vendor/autoloader.php";
     <title>Warehouse</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-        <link rel="stylesheet" href="../../assets/css/style.css">
-        <link rel="icon" href="../../assets/img/image-removebg-preview.png" type="imagen/png">
+    <link rel="stylesheet" href="../../assets/css/style.css">
+    <link rel="icon" href="../../assets/img/image-removebg-preview.png" type="imagen/png">
     <title>Buscar</title>
 
 </head>
 
 
 <body>
-<header>
+    <header>
         <div class="container-fluid">
             <div class="row">
                 <nav class="navbar navbar-expand-sm navbar-dark">
@@ -36,7 +36,7 @@ require_once "../../vendor/autoloader.php";
                         <ul class=" dropdown-menu">
                             <li><a class="dropdown-item" href="warehouseIncidence.php">Incidencia</a></li>
                             <li><a class="dropdown-item" href="warehouseNewOrder.php">Pedir</a></li>
-                            <li><a class="dropdown-item" href="">Pedidos</a></li>
+                            <li><a class="dropdown-item" href="warehouseTiendaConcreta.php">Pedidos</a></li>
                         </ul>
                     </div>
                 </nav>
@@ -45,26 +45,26 @@ require_once "../../vendor/autoloader.php";
     </header>
 
     <div class="container-fluid">
-  <form class="form-inline">
-    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-  </form>
-</div>
-
-
-<footer>
-        <div class="container-fluid" style="background-color: #4D6E9B; margin-top:100px;position:fixed;">
-            <div class="row">
-				<div class="navbar navbar-fixed-bottom">
-                <div class="col-12 text-center">
-				<p>Derechos reservados &copy; 2023</p>
-                <p><a href="../../assets/privacity/privacityPolitic.pdf" target="_blank">Politica de privacidad</a></p>
-                </div>
-				</div>
-            </div>
-        </div>
-    </footer>
+        <form class="form-inline">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+    </div>
 
 </body>
+<footer>
+    <div class="container-fluid" style="background-color: #4D6E9B; margin-top:100px;position:fixed;">
+        <div class="row">
+            <div class="navbar navbar-fixed-bottom">
+                <div class="col-12 text-center">
+                    <p>Derechos reservados &copy; 2023</p>
+                    <p><a href="../../assets/privacity/privacityPolitic.pdf" target="_blank">Politica de privacidad</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 
 </html>
