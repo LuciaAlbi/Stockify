3fe7f975d25bff9985f217b90164e24956c88813
AlvaroGalvezBarba <122776088+AlvaroGalvezBarba@users.noreply.github.com>
2023-05-11 10:18:04 +0200

    update createentrance

--- Login/pages/Almacen/createEntrance.php
+++ Login/pages/Almacen/createEntrance.php
@@ -25,17 +25,31 @@
             </div>
         </div>
     </header>
-	<footer>
-		<div class="container-fluid">
-			<div class="row align-item-center">
-				<div class="col-6 offset-5">
-					<p> Derechos reservados &copy; 2023
-                        <a href="../../assets/privacity/privacityPolitic.pdf" target="_blank">Politica de privacidad</p></a>
-				</div>
-			</div>
-		</div>
-	</footer>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <div class="container">
+        <form>
+            <div class="form-group">
+                <div class="row">
+                    <div class="col-3"><input type="text" class="form-control" id="titulo" placeholder="Ref. Producto"></div>
+                    <div class="col-3"><input type="text" class="form-control" id="titulo" placeholder="Cantidad"></div>
+                    <div class="col-3"><input type="text" class="form-control" id="titulo" placeholder="Fecha"></div>
+                    <div class="col-3"><input type="text" class="form-control" id="titulo" placeholder="Codigo proveedor"></div>
+                </div>
+            </div>
+            <button type="submit" class="btn btn-primary">Realizar </button>
+        </form>
+    </div>
+    <footer>
+        <div class="container-fluid">
+            <div class="row align-item-center">
+                <div class="col-6 offset-5">
+                    <p> Derechos reservados &copy; 2023
+                        <a href="../../assets/privacity/privacityPolitic.pdf" target="_blank">Politica de privacidad
+                    </p></a>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 
 </html>
