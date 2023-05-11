d1c1465c9453f90db40bca7858f00d44a5fd3b8e
EmHeGe <emhege@floridauniversitaria.es>
2023-05-11 13:16:01 +0200

    A Santa no le va el puto git

---
+++ empleado.sql
@@ -0,0 +1,13 @@
+
+INSERT INTO empleados (userId, securePwd, userPwd, nombre, almacen_id)
+VALUES (001, '1234', '$2y$10$hHcBvCH5nC2.S3pVECuJre5bftuengicz7LFAGn/KO/shHzasvNQy', 'Fulgencio Rodríguez', 1);
+
+INSERT INTO empleados (userId, securePwd, userPwd, nombre, almacen_id)
+VALUES (002, '1111', '$2y$10$nJWPXs.4vQQ9Poa2rSVhWeW/EM3KsMKvYV/hgNyy7oTqjHdJtHulO', 'Soledad Bucaramanga', 2);
+
+INSERT INTO empleados (userId, securePwd, userPwd, nombre, almacen_id)
+VALUES (003, '2222', '$2y$10$VvE/EfZM5xOEhA7SVLyhcOepO.TcZd/YHXiW1d9CSFBIizZTrSd2e', 'Roberto Alojero', 3);
+
+INSERT INTO empleados (userId, securePwd, userPwd, nombre, almacen_id)
+VALUES (004, '3333', '$2y$10$2sx.P.EkoPkh2/YdMhmB2.B4I13HwfrGDcoTiOeqVBDG2kT5HBjra', 'Salvador Smith', 4);
+
