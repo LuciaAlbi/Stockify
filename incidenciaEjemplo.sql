463ec83ea03115fbc568aa366f8213ba4aee118d
EmHeGe <emhege@floridauniversitaria.es>
2023-05-11 12:19:49 +0200

    Incidencia de ejemplo para chequear que hace cosas

---
+++ incidenciaEjemplo.sql
@@ -0,0 +1,2 @@
+INSERT INTO incidencias (fecha_hora, descripcion, almacen_id, ref) 
+VALUES ('2023-05-11 10:30:00','El Manolo ha atropellado a Guillermo. Es la tercera vez este mes y encima han roto una nevera',1,'00001');
