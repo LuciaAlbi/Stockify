f26c526679f59e5cc01ed28c70bafb6e069fa4df
EmHeGe <emhege@floridauniversitaria.es>
2023-05-11 10:17:52 +0200

    sql DDL almacen ubi userPWD

---
+++ sqlAlmacen.sql
@@ -0,0 +1,4 @@
+INSERT INTO Almacen (nombre, cp, tlf, capacidad, direccion) VALUES ('Almacen central', '46009', '963661177', '500', 'C/Ejemplo, numero 36');
+INSERT INTO Almacen (nombre, cp, tlf, capacidad, direccion) VALUES ('Tienda Paterna', '46980', '963762178', '50', 'C/Falsa, numero 3');
+INSERT INTO Almacen (nombre, cp, tlf, capacidad, direccion) VALUES ('Almacen Catarroja', '46470', '963661177', '25', 'Av/Ejemplo, numero 6');
+INSERT INTO Almacen (nombre, cp, tlf, capacidad, direccion) VALUES ('Almacen Puçol', '46530', '963661177', '50', 'Av/Falsa, numero 100');
---
+++ sqlUbicacion.sql
@@ -0,0 +1,113 @@
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '1', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '1', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '1', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '1', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '1', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '1', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '1', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '1', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '1', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '1', '1');
+
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '2', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '2', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '2', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '2', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '2', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '2', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '2', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '2', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '2', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '2', '1');
+
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '3', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '3', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '3', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '3', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '3', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '3', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '3', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '3', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '3', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '3', '1');
+
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '002', '1', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '002', '1', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '002', '1', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '002', '1', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '002', '1', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '002', '1', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '002', '1', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '002', '1', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '002', '1', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '002', '1', '1');
+
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '002', '2', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '002', '2', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '002', '2', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '002', '2', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '002', '2', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '002', '2', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '002', '2', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '002', '2', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '002', '2', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '002', '2', '1');
+
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '002', '3', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '002', '3', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '002', '3', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '002', '3', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '002', '3', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '002', '3', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '002', '3', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '002', '3', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '002', '3', '1');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '002', '3', '1');
+
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '1', '2');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '1', '2');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '1', '2');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '1', '2');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '1', '2');
+
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '2', '2');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '2', '2');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '2', '2');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '2', '2');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '2', '2');
+
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '3', '2');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '3', '2');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '3', '2');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '3', '2');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '3', '2');
+
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '002', '1', '2');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '002', '1', '2');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '002', '1', '2');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '002', '1', '2');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '002', '1', '2');
+
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '1', '3');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '1', '3');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '1', '3');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '1', '3');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '1', '3');
+
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '2', '3');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '2', '3');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '2', '3');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '2', '3');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '2', '3');
+
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '3', '3');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '3', '3');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '3', '3');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '3', '3');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '001', '3', '3');
+
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '002', '1', '3');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '002', '1', '3');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '002', '1', '3');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '002', '1', '3');
+INSERT INTO Ubicacion (zona, pasillo, estanteria, altura, id_almacen) VALUES ('A', '001', '002', '1', '3');
