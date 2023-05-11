4f5193d5aca78f48a5662302ede1edeb2c907879
EmHeGe <emhege@floridauniversitaria.es>
2023-05-11 12:43:31 +0200

    Create tieneEjemplo.sql

---
+++ tieneEjemplo.sql
@@ -0,0 +1,10 @@
+INSERT INTO tiene (cant, almacen_id, ref) 
+VALUES (10,1,'00010');
+INSERT INTO tiene (cant, almacen_id, ref) 
+VALUES (2,1,'00004');
+INSERT INTO tiene (cant, almacen_id, ref) 
+VALUES (34,2,'00003');
+INSERT INTO tiene (cant, almacen_id, ref) 
+VALUES (2,3,'00004');
+INSERT INTO tiene (cant, almacen_id, ref) 
+VALUES (2,4,'00005');
