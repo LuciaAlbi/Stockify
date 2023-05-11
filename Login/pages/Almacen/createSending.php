0a37094802d8d1ebba07280b0121aa69e207a4f7
AlvaroGalvezBarba <122776088+AlvaroGalvezBarba@users.noreply.github.com>
2023-05-11 09:52:27 +0200

    update landing

--- Login/pages/Almacen/createSending.php
+++ Login/pages/Almacen/createSending.php
@@ -8,7 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="theme-color" content="#4D6E9B">
-    <title>Incidence</title>
+    <title>Crear salida</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="icon" href="../../assets/img/image-removebg-preview.png" type="imagen/png">
     <link href="../../assets/css/style.css" rel="stylesheet">
@@ -28,19 +28,7 @@
         </div>
     </header>
 
-    <div class="container">
-  <form>
-    <div class="form-group">
-      <label for="titulo">Título</label>
-      <input type="text" class="form-control" id="titulo" placeholder="Ingrese un título para el titulo">
-    </div>
-    <div class="form-group">
-      <label for="descripcion">Descripción</label>
-      <textarea class="form-control" id="descripcion" rows="3" placeholder="Ingrese una descripción para el pedido"></textarea>
-    </div>
-    <button type="submit" class="btn btn-primary">Realizar pedido</button>
-  </form>
-</div>
+
 <footer>
 		<div class="container-fluid">
 			<div class="row align-item-center">
@@ -53,5 +41,4 @@
 	</footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
-
 </html>
--- Login/pages/Almacen/entrance.php
+++ Login/pages/Almacen/entrance.php
@@ -6,21 +6,31 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="theme-color" content="#4D6E9B">
-    <title>Document</title>
+    <title>Entrada</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="icon" href="../../assets/img/image-removebg-preview.png" type="imagen/png">
     <link href="../../assets/css/style.css" rel="stylesheet">
 </head>
 
 <body>
-    <header>
+<header>
         <div class="container-fluid">
             <div class="row">
                 <nav class="navbar navbar-expand-sm navbar-dark">
-                    <a class="navbar-brand col-1" href="Landing.php">
+                    <a class="navbar-brand col-1" href="landing.php">
                         <img src="../../assets/img/image-removebg-preview.png" alt="Avatar Logo" style="width:50px;">
                     </a>
-                    <h2 class="col-10 text-center">La casa del electrodomestico</h2>
+                    <h2 class="col-8 offset-1 text-center">La casa del electrodomestico</h2>
+                    <div class="dropdown col-1">
+                        <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown">
+                        <span>Crear entrada/Incidencia/Salida</span>
+                        </button>
+                        <ul class=" dropdown-menu">
+                            <li><a class="dropdown-item" href="createEntrance.php">Crear entrada</a></li>
+                            <li><a class="dropdown-item" href="Incidence.php">Incidencia</a></li>
+                            <li><a class="dropdown-item" href="sending.php">Salida</a></li>
+                        </ul>
+                    </div>
                 </nav>
             </div>
         </div>
