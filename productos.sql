b5f9fd0a09c17d99a4bc9d393037cb1e88e53da2
EmHeGe <emhege@floridauniversitaria.es>
2023-05-11 10:31:02 +0200

    subo productos actualizo pwd

---
+++ productos.sql
@@ -0,0 +1,77 @@
+INSERT INTO productos (Ref, p_compra, p_venta, marca, modelo, peso, f_rota, dimensiones, proveedor, color)
+VALUES ('00005', 60, 120, 'Orbegozo', 'Microondas 700W', 10, 'Alto', '450 x 345 x 260 mm', 'BetaCorp', 'Blanco');
+
+INSERT INTO productos (Ref, p_compra, p_venta, marca, modelo, peso, f_rota, dimensiones, proveedor, color)
+VALUES ('00006', 280, 499, 'LG', 'TV OLED 4K 65"', 22, 'Medio', '1449 x 832 x 47 mm', 'Gamma Inc.', 'Plateado');
+
+INSERT INTO productos (Ref, p_compra, p_venta, marca, modelo, peso, f_rota, dimensiones, proveedor, color)
+VALUES ('00007', 70, 135, 'Taurus', 'Cafetera goteo 12 tazas', 2, 'Alto', '220 x 270 x 370 mm', 'ACME Inc.', 'Negro');
+
+INSERT INTO productos (Ref, p_compra, p_venta, marca, modelo, peso, f_rota, dimensiones, proveedor, color)
+VALUES ('00008', 300, 549, 'Samsung', 'Frigorífico Side-by-Side', 100, 'Bajo', '1780 x 912 x 716 mm', 'BetaCorp', 'Negro');
+
+INSERT INTO productos (Ref, p_compra, p_venta, marca, modelo, peso, f_rota, dimensiones, proveedor, color)
+VALUES ('00009', 120, 199, 'Orbegozo', 'Ventilador de pie 40cm', 4, 'Medio', '430 x 430 x 1220 mm', 'Gamma Inc.', 'Negro');
+
+INSERT INTO productos (Ref, p_compra, p_venta, marca, modelo, peso, f_rota, dimensiones, proveedor, color)
+VALUES ('00010', 150, 299, 'Bosch', 'Cocina de gas 4 fuegos', 30, 'Bajo', '600 x 600 x 850 mm', 'ACME Inc.', 'Blanco');
+
+INSERT INTO productos (Ref, p_compra, p_venta, marca, modelo, peso, f_rota, dimensiones, proveedor, color)
+VALUES ('00011', 80, 149, 'Taurus', 'Plancha de vapor 2200W', 1.5, 'Medio', '285 x 130 x 130 mm', 'BetaCorp', 'Azul');
+
+INSERT INTO productos (Ref, p_compra, p_venta, marca, modelo, peso, f_rota, dimensiones, proveedor, color)
+VALUES ('00012', 350, 599, 'LG', 'Frigorífico French Door', 120, 'Bajo', '1790 x 912 x 758 mm', 'Gamma Inc.', 'Inoxidable');
+
+INSERT INTO productos (Ref, p_compra, p_venta, marca, modelo, peso, f_rota, dimensiones, proveedor, color)
+VALUES ('00013', 25, 49, 'Orbegozo', 'Placa de cocina eléctrica', 5, 'Alto', '520 x 290 x 70 mm', 'ACME Inc.', 'Negro');
+
+INSERT INTO productos (Ref, p_compra, p_venta, marca, modelo, peso, f_rota, dimensiones, proveedor, color)
+VALUES ('00014', 90, 169, 'Bosch', 'Batidora de mano 800W', 1, 'Medio', '200 x 150 x 300 mm', 'BetaCorp', 'Rojo');
+
+INSERT INTO productos (Ref, p_compra, p_venta, marca, modelo, peso, f_rota, dimensiones, proveedor, color)
+VALUES ('00015', 200, 399, 'Samsung', 'Frigorífico Americano', 150, 'Bajo', '1790 x 912 x 758 mm', 'Gamma Inc.', 'Inoxidable');
+
+INSERT INTO productos (Ref, p_compra, p_venta, marca, modelo, peso, f_rota, dimensiones, proveedor, color)
+VALUES ('00016', 30, 59, 'Taurus', 'Tostador de pan 2 rebanadas', 1, 'Alto', '190 x 170 x 100 mm', 'ACME Inc.', 'Negro');
+
+INSERT INTO productos (Ref, p_compra, p_venta, marca, modelo, peso, f_rota, dimensiones, proveedor, color)
+VALUES ('00017', 400, 749, 'LG', 'TV FHD 75"', 30, 'Medio', '1687 x 965 x 54 mm', 'ACME Inc.', 'Negro');
+
+INSERT INTO productos (Ref, p_compra, p_venta, marca, modelo, peso, f_rota, dimensiones, proveedor, color)
+VALUES ('00018', 220, 399, 'Bosch', 'Robot de cocina 1000W', 5, 'Alto', '320 x 320 x 440 mm', 'Gamma Inc.', 'Plateado');
+
+INSERT INTO productos (Ref, p_compra, p_venta, marca, modelo, peso, f_rota, dimensiones, proveedor, color)
+VALUES ('00019', 60, 119, 'Orbegozo', 'Plancha de asar 2000W', 3, 'Medio', '460 x 280 x 80 mm', 'BetaCorp', 'Negro');
+
+INSERT INTO productos (Ref, p_compra, p_venta, marca, modelo, peso, f_rota, dimensiones, proveedor, color)
+VALUES ('00020', 800, 1399, 'Samsung', 'TV QLED 8K 98"', 60, 'Medio', '2178 x 1245 x 70 mm', 'ACME Inc.', 'Negro');
+
+INSERT INTO productos (Ref, p_compra, p_venta, marca, modelo, peso, f_rota, dimensiones, proveedor, color)
+VALUES ('00021', 250, 449, 'Orbegozo', 'Batidora de vaso BV 6500', 3, 'Bajo', '225 x 225 x 410 mm', 'BetaCorp', 'Acero inoxidable');
+
+INSERT INTO productos (Ref, p_compra, p_venta, marca, modelo, peso, f_rota, dimensiones, proveedor, color)
+VALUES ('00022', 400, 749, 'Samsung', 'TV QLED 4K 75"', 30, 'Bajo', '1672 x 969 x 64 mm', 'Gamma Inc.', 'Negro');
+
+INSERT INTO productos (Ref, p_compra, p_venta, marca, modelo, peso, f_rota, dimensiones, proveedor, color)
+VALUES ('00023', 150, 299, 'Philips', 'Plancha de vapor GC5039/30', 1.5, 'Alto', '310 x 165 x 160 mm', 'ACME Inc.', 'Negro');
+
+INSERT INTO productos (Ref, p_compra, p_venta, marca, modelo, peso, f_rota, dimensiones, proveedor, color)
+VALUES ('00024', 500, 899, 'Bosch', 'Horno multifunción Serie 4', 35, 'Medio', '595 x 594 x 548 mm', 'BetaCorp', 'Acero inoxidable');
+
+INSERT INTO productos (Ref, p_compra, p_venta, marca, modelo, peso, f_rota, dimensiones, proveedor, color)
+VALUES ('00025', 80, 159, 'Orbegozo', 'Exprimidor eléctrico EP 4000', 2, 'Alto', '225 x 225 x 350 mm', 'Gamma Inc.', 'Blanco');
+
+INSERT INTO productos (Ref, p_compra, p_venta, marca, modelo, peso, f_rota, dimensiones, proveedor, color)
+VALUES ('00026', 120, 249, 'Taurus', 'Batidora amasadora Prima', 2.5, 'Bajo', '240 x 370 x 350 mm', 'ACME Inc.', 'Blanco');
+
+INSERT INTO productos (Ref, p_compra, p_venta, marca, modelo, peso, f_rota, dimensiones, proveedor, color)
+VALUES ('00027', 80, 169, 'Philips', 'Plancha de vapor ComfortTouch', 1.2, 'Medio', '290 x 120 x 150 mm', 'BetaCorp', 'Azul');
+
+INSERT INTO productos (Ref, p_compra, p_venta, marca, modelo, peso, f_rota, dimensiones, proveedor, color)
+VALUES ('00028', 300, 549, 'Bosch', 'Horno multifunción Serie 4', 25, 'Bajo', '595 x 594 x 548 mm', 'Gamma Inc.', 'Negro');
+
+INSERT INTO productos (Ref, p_compra, p_venta, marca, modelo, peso, f_rota, dimensiones, proveedor, color)
+VALUES ('00029', 40, 79, 'Taurus', 'Sandwichera Grill', 1.5, 'Alto', '240 x 240 x 90 mm', 'ACME Inc.', 'Negro');
+
+INSERT INTO productos (Ref, p_compra, p_venta, marca, modelo, peso, f_rota, dimensiones, proveedor, color)
+VALUES ('00030', 600, 1099, 'Samsung', 'TV LED 8K 65"', 20, 'Medio', '1449 x 829 x 25 mm', 'BetaCorp', 'Negro');
---
+++ userPwd.php
@@ -0,0 +1,14 @@
+<?php
+echo "1234 \n";
+echo password_hash("1234", PASSWORD_BCRYPT);
+echo "\n";
+echo "1111 \n";
+echo password_hash("1111", PASSWORD_BCRYPT);
+echo "\n";
+echo "2222 \n";
+echo password_hash("2222", PASSWORD_BCRYPT);
+echo "\n";
+echo "3333 \n";
+echo password_hash("3333", PASSWORD_BCRYPT);
+
+
