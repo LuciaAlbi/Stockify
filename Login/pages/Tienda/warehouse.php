48645581bc797759df2c317dd37179309c5a07da
AlvaroGalvezBarba <122776088+AlvaroGalvezBarba@users.noreply.github.com>
2023-05-12 12:30:08 +0200

    Update warehouse.php

--- Login/pages/Tienda/warehouse.php
+++ Login/pages/Tienda/warehouse.php
@@ -1,8 +1,9 @@
 <?php
 
 require_once "../../vendor/autoloader.php";
-
 ?>
+
+
 <!DOCTYPE html>
 <html>
 
@@ -10,14 +11,11 @@ require_once "../../vendor/autoloader.php";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Warehouse</title>
+    <title>Producto</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" href="../../assets/css/style.css">
     <link rel="icon" href="../../assets/img/image-removebg-preview.png" type="imagen/png">
-    <title>Buscar</title>
-
 </head>
-
 
 <body>
     <header>
@@ -28,14 +26,14 @@ require_once "../../vendor/autoloader.php";
                         <img src="../../assets/img/image-removebg-preview.png" alt="Avatar Logo" style="width:50px;">
                     </a>
                     <h2 class="col-9 text-center">La casa del electrodomestico</h2>
-                    <div class="dropdown col-5">
+                    <div class="dropdown col-1">
                         <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown">
-                            Incidencia/Pedir/Pedidos
+                            Incidencia/Entrada/Saldia
                         </button>
-                        <ul class=" dropdown-menu">
-                            <li><a class="dropdown-item" href="warehouseIncidence.php">Incidencia</a></li>
-                            <li><a class="dropdown-item" href="warehouseNewOrder.php">Pedir</a></li>
-                            <li><a class="dropdown-item" href="warehouseTiendaConcreta.php">Pedidos</a></li>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="Incidence.php">Incidencia</a></li>
+                            <li><a class="dropdown-item" href="entrance.php">Entrada productos</a></li>
+                            <li><a class="dropdown-item" href="sending.php">Salida productos</a></li>
                         </ul>
                     </div>
                 </nav>
@@ -45,25 +43,26 @@ require_once "../../vendor/autoloader.php";
 
     <div class="container-fluid">
         <form class="form-inline">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
-    </div>
-
-</body>
-<footer>
-    <div class="container-fluid" style="background-color: #4D6E9B; margin-top:100px;position:fixed;">
-        <div class="row">
-            <div class="navbar navbar-fixed-bottom">
-                <div class="col-12 text-center">
-                    <p>Derechos reservados &copy; 2023</p>
-                    <p><a href="../../assets/privacity/privacityPolitic.pdf" target="_blank">Politica de privacidad</a>
-                    </p>
-                </div>
+            <div class="input-group">
+                <input ID="txtPassword" type="Password" class="form-control" placeholder="Search">
+                <span class="input-group-btn">
+                    <button id="show_password" class="btn btn-primary" type="button">
+                        Search<span class="fa fa-eye-slash icon"></span>
+                    </button>
+                </span>
             </div>
-        </div>
-    </div>
-</footer>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+            <footer>
+                <div class="container-fluid">
+                    <div class="row align-item-center">
+                        <div class="col-6 offset-5">
+                            <p> Derechos reservados &copy; 2023
+                                <a href="../../assets/privacity/privacityPolitic.pdf" target="_blank">Politica de privacidad
+                            </p></a>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</body>
 
 </html>
