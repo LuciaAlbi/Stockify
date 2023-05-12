652752b023a56acaf5c3b732255092c54b0412ef
AlvaroGalvezBarba <122776088+AlvaroGalvezBarba@users.noreply.github.com>
2023-05-12 12:39:57 +0200

    Create mueve.sql

---
+++ mueve.sql
@@ -0,0 +1,19 @@
+USE stockify;
+INSERT INTO mueve (fecha, id_almacen, idProducto,cant, realizado) 
+VALUES (2023-05-11,1,'00002',10,0);
+INSERT INTO mueve (fecha, id_almacen, idProducto,cant, realizado) 
+VALUES (2023-08-13,2,'00003',20,0);
+INSERT INTO mueve (fecha, id_almacen, idProducto,cant,  realizado) 
+VALUES (2023-01-24,3,'00004',12,0);
+INSERT INTO mueve (fecha, id_almacen, idProducto,cant,  realizado) 
+VALUES (2023-02-29,4,'00005',1,0);
+INSERT INTO mueve (fecha, id_almacen, idProducto,cant,  realizado) 
+VALUES (2023-04-30,1,'00006',2,0);
+INSERT INTO mueve (fecha, id_almacen, idProducto,cant,  realizado) 
+VALUES (2023-01-25,1,'00007',45,0);
+INSERT INTO mueve (fecha, id_almacen, idProducto,cant,  realizado) 
+VALUES (2023-03-21,1,'00008',32,0);
+INSERT INTO mueve (fecha, id_almacen, idProducto,cant,  realizado) 
+VALUES (2023-02-27,1,'00009',10,0);
+INSERT INTO mueve (fecha, id_almacen, idProducto,cant,  realizado) 
+VALUES (2023-01-20,1,'00010',8,0);
