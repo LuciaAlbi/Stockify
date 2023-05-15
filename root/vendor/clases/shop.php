8d923c44100813b7a661cd9c9dece72d73d40a7f
LuciaAlbi <97888214+LuciaAlbi@users.noreply.github.com>
2023-05-15 09:26:01 +0200

    Update shop.php

--- root/vendor/clases/shop.php
+++ root/vendor/clases/shop.php
@@ -6,7 +6,11 @@ class Shop extends Connection
 {
     public function verShop()
     {
-        $sql = "SELECT * FROM almacen";
+        $sql = "SELECT p.ref, p.marca, p.modelo, u.zona, u.pasillo, u.estanteria, u.altura
+        FROM productos p
+        JOIN esta e ON p.ref = e.ref
+        JOIN ubicacion u ON e.Id_ubi = u.Id_ubi
+        WHERE e.almacen_id = 1;";
         $result = $this->conn->query($sql);
         if ($result->num_rows > 0) {
             $row = array();
