8383fc9dcfd6f181994903438455c900d5a3c187
AlvaroGalvezBarba <122776088+AlvaroGalvezBarba@users.noreply.github.com>
2023-05-15 09:28:32 +0200

    Update 7incidenciaEjemplo.sql

--- 7incidenciaEjemplo.sql
+++ 7incidenciaEjemplo.sql
@@ -1,9 +1,15 @@
 USE stockify;
 INSERT INTO incidencias (fecha_hora, descripcion, almacen_id, ref) 
+<<<<<<< Updated upstream
 VALUES ('2023-05-11 10:30:00','El Manolo ha atropellado a Guillermo. Es la tercera vez este mes y encima han roto una nevera',1,'00001');
 INSERT INTO incidencias (fecha_hora, descripcion, almacen_id, ref) 
 VALUES ('2023-05-11 10:50:00','El Manolo ha atropellado a Guillermo. Es la cuarta vez este mes y encima han roto una nevera',2,'00002');
 INSERT INTO incidencias (fecha_hora, descripcion, almacen_id, ref) 
 VALUES ('2023-05-11 11:00:00','El Manolo ha atropellado a Guillermo. Es la quinta vez este mes y encima han roto una nevera',3,'00003');
+=======
+VALUES ('2023-05-11 10:50:00','El Manolo ha atropellado a Guillermo. Es la cuarta vez este mes y encima han roto una lavadora',2,'00002');
+INSERT INTO incidencias (fecha_hora, descripcion, almacen_id, ref) 
+VALUES ('2023-05-11 11:00:00','El Manolo ha atropellado a Guillermo. Es la quinta vez este mes y encima han roto una ordenador',3,'00003');
+>>>>>>> Stashed changes
 INSERT INTO incidencias (fecha_hora, descripcion, almacen_id, ref) 
 VALUES ('2023-05-11 12:30:00','El Manolo ha atropellado a Guillermo. Es la sexta vez este mes y encima han roto una nevera',4,'00004');
