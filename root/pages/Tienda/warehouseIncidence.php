af2447ae82c55b59358a00ded57459a8875bdeb1
Adrian78q <122776156+Adrian78q@users.noreply.github.com>
2023-05-15 12:51:36 +0200

    Update warehouseIncidence.php

--- root/pages/Tienda/warehouseIncidence.php
+++ root/pages/Tienda/warehouseIncidence.php
@@ -66,13 +66,12 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 <footer>
-    <div class="container-fluid" style="background-color: #4D6E9B; margin-top:100px;position:fixed;">
+    <div class="container-fluid">
         <div class="row">
-            <div class="navbar navbar-fixed-bottom">
-                <div class="col-12 text-center">
-                    <p>Derechos reservados &copy; 2023</p>
-                    <p><a href="../../assets/privacity/privacityPolitic.pdf" target="_blank">Politica de privacidad</a></p>
-                </div>
+            <div class="col-12 text-center">
+                <p> Derechos reservados &copy; 2023
+                    <a href="../../assets/privacity/privacityPolitic.pdf" target="_blank">Politica de privacidad
+                </p></a>
             </div>
         </div>
     </div>
