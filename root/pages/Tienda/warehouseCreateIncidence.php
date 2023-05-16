8ad1bfb4bf06ce5c2c0dccd3300ae1c001be3c40
LuciaAlbi <97888214+LuciaAlbi@users.noreply.github.com>
2023-05-16 11:35:52 +0200

    Update warehouseCreateIncidence.php

--- root/pages/Tienda/warehouseCreateIncidence.php
+++ root/pages/Tienda/warehouseCreateIncidence.php
@@ -2,64 +2,51 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="theme-color" content="#4D6E9B">
-    <title>Create Incidence</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <link rel="icon" href="../../assets/img/image-removebg-preview.png" type="imagen/png">
-    <link href="../../assets/css/style.css" rel="stylesheet">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="theme-color" content="#4D6E9B">
+  <title>Create Incidence</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  <link rel="icon" href="../../assets/img/image-removebg-preview.png" type="imagen/png">
+  <link href="../../assets/css/style.css" rel="stylesheet">
 </head>
+
 <body>
-    <header>
-        <div class="container-fluid">
-            <div class="row">
-                <nav class="navbar navbar-expand-sm navbar-dark">
-                    <a class="navbar-brand col-1" href="warehouse.php">
-                        <img src="../../assets/img/image-removebg-preview.png" alt="Avatar Logo" style="width:50px;">
-                    </a>
-                    <h2 class="col-10 text-center">La casa del electrodomestico</h2>
-                </nav>
-            </div>
-        </div>
-    </header>
-    <div class="container">
-  <form id="incidenceForm" action="warehouseIncidence.php" method="POST">
-    <div class="form-group">
-      <label for="titulo">Título</label>
-      <input type="text" class="form-control" id="titulo" name="titulo" placeholder="Ingrese un título para la incidencia">
+  <header>
+    <div class="container-fluid">
+      <div class="row">
+        <nav class="navbar navbar-expand-sm navbar-dark">
+          <a class="navbar-brand col-1" href="warehouse.php">
+            <img src="../../assets/img/image-removebg-preview.png" alt="Avatar Logo" style="width:50px;">
+          </a>
+          <h2 class="col-10 text-center">La casa del electrodomestico</h2>
+        </nav>
+      </div>
     </div>
-    <div class="form-group">
-      <label for="descripcion">Descripción</label>
-      <textarea class="form-control" id="descripcion" name="descripcion" rows="3" placeholder="Descripción para la incidencia"></textarea>
-    </div>
-    <button type="submit" class="btn btn-primary" id="submitButton" disabled>Realizar incidencia</button>
-  </form>
-</div>
-<script>
-  const tituloInput = document.getElementById('titulo');
-  const descripcionInput = document.getElementById('descripcion');
-  const submitButton = document.getElementById('submitButton');
+  </header>
+  <div class="container">
+    <form id="incidenceForm" action="warehouseIncidence.php" method="POST">
+      <div class="form-group">
+        <label for="titulo">Título</label>
+        <input type="text" class="form-control" id="titulo" name="titulo"
+          placeholder="Ingrese un título para la incidencia">
+      </div>
+      <div class="form-group">
+        <label for="descripcion">Descripción</label>
+        <textarea class="form-control" id="descripcion" name="descripcion" rows="3"
+          placeholder="Descripción para la incidencia"></textarea>
+      </div>
+      <button type="submit" class="btn btn-primary" id="submitButton" disabled>Realizar incidencia</button>
+    </form>
+  </div>
 
-  tituloInput.addEventListener('input', toggleSubmitButton);
-  descripcionInput.addEventListener('input', toggleSubmitButton);
-
-  function toggleSubmitButton() {
-    if (tituloInput.value.trim() !== '' && descripcionInput.value.trim() !== '') {
-      submitButton.disabled = false;
-    } else {
-      submitButton.disabled = true;
-    }
-  }
-
-  document.querySelector('#incidenceForm').addEventListener('submit', function(event) {
-    var titulo = document.getElementById('titulo').value;
-    var descripcion = document.getElementById('descripcion').value;
-  });
-</script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+    crossorigin="anonymous"></script>
 </body>
 <footer>
   <div class="container-fluid">
@@ -72,4 +59,5 @@
     </div>
   </div>
 </footer>
+
 </html>
