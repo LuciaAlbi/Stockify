9bcf5fe667223969e949d7dfa058694e906f8a86
LuciaAlbi <97888214+LuciaAlbi@users.noreply.github.com>
2023-05-16 10:12:04 +0200

    Update searchbar.js

--- root/assets/js/searchbar.js
+++ root/assets/js/searchbar.js
@@ -1,6 +1,21 @@
-function searchObject() {
+window.addEventListener('DOMContentLoaded', function () {
+    // Obtener los parámetros de búsqueda de la URL
+    var urlParams = new URLSearchParams(window.location.search);
+    var searchValue = urlParams.get('search');
+  
+    // Establecer el valor de búsqueda en el campo de entrada
+    document.getElementById('searchbar').value = searchValue;
+  
+    // Realizar la búsqueda inicial
+    searchObject();
+  });
+  
+  function searchObject() {
     var input = document.getElementById('searchbar').value.toLowerCase();
     var rows = document.querySelectorAll('#list tr');
+  
+    // Obtener los parámetros de búsqueda actuales de la URL
+    var urlParams = new URLSearchParams(window.location.search);
   
     for (var i = 0; i < rows.length; i++) {
       var row = rows[i];
@@ -22,5 +37,7 @@ function searchObject() {
         row.style.display = 'none';
       }
     }
-  }
   
+    // Actualizar el parámetro de búsqueda en la URL
+    url
+  }
