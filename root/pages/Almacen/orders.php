6119e0c21d880e03acbd16915522a96186106968
AlvaroGalvezBarba <122776088+AlvaroGalvezBarba@users.noreply.github.com>
2023-05-16 13:21:04 +0200

    Revert "Revert "Merge branch 'Code' of https://github.com/LuciaAlbi/Stockify into Code""
    This reverts commit 409ae034d33fed87ed6b5d1045916406391de3ec.

--- root/pages/Almacen/orders.php
+++ root/pages/Almacen/orders.php
@@ -6,50 +6,49 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="theme-color" content="#4D6E9B">
-    <title>Salida</title>
+    <title>Oredenes</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="icon" href="../../assets/img/image-removebg-preview.png" type="imagen/png">
     <link href="../../assets/css/style.css" rel="stylesheet">
 </head>
 
 <body>
-    <header>
+<header>
         <div class="container-fluid">
             <div class="row">
                 <nav class="navbar navbar-expand-sm navbar-dark">
                     <a class="navbar-brand col-1" href="landing.php">
                         <img src="../../assets/img/image-removebg-preview.png" alt="Avatar Logo" style="width:50px;">
                     </a>
-                    <h2 class="col-8 offset-1 text-center">La casa del electrodomestico</h2>
-                    <div class="dropdown col-1">
-                        <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown">
-                            <span>Crear salida/Incidencia/Entrada</span>
+                    <h2 class="col-lg-4 offset-lg-3 col-md-7 col-sm-6">La casa del electrodomestico</h2>
+                    <div class="navbar-brand col-2 offset-1">
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
                         </button>
-                        <ul class=" dropdown-menu">
-                            <li><a class="dropdown-item" href="createSending.php">Crear salida</a></li>
-                            <li><a class="dropdown-item" href="Incidence.php">Incidencia</a></li>
-                            <li><a class="dropdown-item" href="entrance.php">Entrada</a></li>
-                        </ul>
+                        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                            <ul class="navbar-nav">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="createSending.php">Crear salidas</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="entrance.php">Entrada</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="Incidence.php">Incidencias</a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </nav>
             </div>
         </div>
     </header>
+    <section>
     <div class="container">
         <div class="row justify-content-center">
-            <div class="prueba col-4 align-self-center">
+            <div class="prueba col-5 align-self-center">
                 <div class="row justify-content-center"><!-- Se tiene que copiar desde aqui -->
-                    <div class=" prueba1 col-4 align-self-center">
-                        <form id="inci">
-                            <div class="form-group">
-                                <label for="titulo">Ref. Producto/Fecha/Cantidad/Codigo prov</label>
-                                <input type="text" class="form-control" id="titulo" placeholder="" readonly>
-                            </div>
-                        </form>
-                    </div>
-                </div><!-- Hasta aqui para mostrar otra incidencia -->
-                <div class="row justify-content-center"><!-- Se tiene que copiar desde aqui -->
-                    <div class=" prueba1 col-4 align-self-center">
+                    <div class=" prueba1 col-6 align-self-center">
                         <form id="inci">
                             <div class="form-group">
                                 <label for="titulo">Id/Fecha/Cantidad/id Almacen/Ref. Producto/Codigo prov</label>
