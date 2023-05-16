3eb14d5b76365d4a88589f456b0a8dbdcfcec734
LuciaAlbi <97888214+LuciaAlbi@users.noreply.github.com>
2023-05-16 08:45:03 +0200

    Update Product.php

--- root/vendor/clases/Product.php
+++ root/vendor/clases/Product.php
@@ -4,15 +4,15 @@ class Product extends Connection
 {
     public function verProductAlmacen() // ver listado de todos los productos desde base de datos
     {
-        $sql = "SELECT * FROM productos";
+        $sql = "SELECT p.ref, p.marca, p.modelo, u.zona, u.pasillo, u.estanteria, u.altura
+        FROM productos p
+        JOIN esta e ON p.ref = e.ref
+        JOIN ubicacion u ON e.Id_ubi = u.Id_ubi
+        WHERE e.almacen_id = 1;";
         $result = $this->conn->query($sql);
         if ($result->num_rows > 0) {
-            $row = array();
-            while ($rows = $result->fetch_assoc()) {
-                $row[] = $rows;
-            }
-            return $row;
-        } else {
+            return $result->fetch_all();
+        } else {    
             return false;
         }
     }
