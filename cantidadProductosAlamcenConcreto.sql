a50085e671ca705ef1b067dc79335e549bde0f70
EmHeGe <emhege@floridauniversitaria.es>
2023-05-17 11:43:38 +0200

    count

---
+++ cantidadProductosAlamcenConcreto.sql
@@ -0,0 +1,11 @@
+SELECT count(*)
+
+FROM productos p
+
+JOIN esta e ON p.ref = e.ref
+
+JOIN ubicacion u ON e.Id_ubi = u.Id_ubi
+
+WHERE e.almacen_id = 2 AND p.ref = '00003';
+
+ 
--- queryProductosAlamcenConcreto.sql
+++
@@ -1,5 +0,0 @@
-SELECT p.ref, p.marca, p.modelo, u.zona, u.pasillo, u.estanteria, u.altura
-FROM productos p
-JOIN esta e ON p.ref = e.ref
-JOIN ubicacion u ON e.Id_ubi = u.Id_ubi
-WHERE e.almacen_id = 1;
