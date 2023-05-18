c0353c6f61b66930760df2c88da9e487c97055c8
Adrian78q <122776156+Adrian78q@users.noreply.github.com>
2023-05-18 13:00:09 +0200

    Update Movement.php

--- root/vendor/clases/Movement.php
+++ root/vendor/clases/Movement.php
@@ -24,8 +24,18 @@ class Movement extends Connection
     {
         $output = "";
         for ($i = 0; $i < count($product); $i++) {
-            $output .= "<tr>";
-            $output .= $product[$i]["idMOv"] . " / " . $product[$i]["fecha"] . " / " . $product[$i]["cant"] . " / " . $product[$i]["id_almacen"] . " / " . $product[$i]["idProducto"] . " / " . $product[$i]["realizado"] . "</td>";
+            $output .= "<div class='row justify-content-center'>";
+            $output .= "<div class='prueba1 col-4 align-self-center'>";
+            $output .= "<form id='inci'>";
+            $output .= "<div class='form-group'>";
+            $output .= "<label for='titulo'>Ref. Producto / Fecha / Cantidad / Codigo prov </label>";
+            $output .= "<input type='text' style='text-align:center' class='form-control' id='titulo' placeholder='" . $product[$i]["idProducto"] . " / " . $product[$i]["fecha"] . " / " . $product[$i]["cant"] . " / " . $product[$i]["id_almacen"] . "' readonly>";
+            $output .= "</div>";
+            $output .= "</form>";
+            $output .= "</div>";
+            $output .= "</div>";
+            //$output .= "<tr>";
+            //$output .= $product[$i]["idMOv"] . " / " . $product[$i]["fecha"] . " / " . $product[$i]["cant"] . " / " . $product[$i]["id_almacen"] . " / " . $product[$i]["idProducto"] . " / " . $product[$i]["realizado"] . "</td>";
         }
         return $output;
     }
@@ -47,14 +57,14 @@ class Movement extends Connection
     }
     public function drawSending($sending)
     {
-        var_dump($_SESSION);
+        //var_dump($_SESSION);
         $output = "";
         for ($i = 0; $i < count($sending); $i++) {
             $output .= "<div class='row justify-content-center'>";
             $output .= "<div class='prueba1 col-4 align-self-center'>";
             $output .= "<form id='inci'>";
             $output .= "<div class='form-group'>";
-            $output .= "<label for='titulo'>Movimiento/ Fecha/ Cantidad/ Almacen/ IdProducto/ Realizado </label>";
+            $output .= "<label for='titulo'>Movimiento / Fecha / Cantidad / Almacen / IdProducto / Realizado </label>";
             $output .= "<input type='text' style='text-align:center' class='form-control' id='titulo' placeholder='" . $sending[$i]["idMOv"] . " / " . $sending[$i]["fecha"] . " / " . $sending[$i]["cant"] . " / " . $sending[$i]["id_almacen"] . " / " . $sending[$i]["idProducto"] . " / " . $sending[$i]["realizado"] . "' readonly>";
             $output .= "</div>";
             $output .= "</form>";
