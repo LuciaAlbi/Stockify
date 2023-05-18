b07a8c58d657ee2de386b7aabc67d8315cdb0ffd
LuciaAlbi <97888214+LuciaAlbi@users.noreply.github.com>
2023-05-18 08:47:37 +0200

    Update insert.php

--- root/pages/Tienda/insert.php
+++ root/pages/Tienda/insert.php
@@ -1,13 +1,14 @@
-<?php 
+<?php
 require_once "../../vendor/autoloader.php";
 
-$repository = new Inci($_POST['fecha_hora'],
+$repository = new Inci(
+    $_POST['fecha_hora'],
     $_POST['descripcion'],
     $_POST['almacen_id'],
     $_POST['ref']
- );
+);
 try {
-    
+
     $repository->insertInci();
     header('Location: /warehouseIncidence.php');
 } catch (\Throwable $th) {
