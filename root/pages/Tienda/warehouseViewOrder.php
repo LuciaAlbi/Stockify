fc90449f733564ad3b17cfbaeebbe9978450c295
AlvaroGalvezBarba <122776088+AlvaroGalvezBarba@users.noreply.github.com>
2023-05-19 13:24:33 +0200

    Update warehouseViewOrder.php

--- root/pages/Tienda/warehouseViewOrder.php
+++ root/pages/Tienda/warehouseViewOrder.php
@@ -1,5 +1,5 @@
 <?php
-if (session_status() !== PHP_SESSION_ACTIVE)session_start();
+if (session_status() !== PHP_SESSION_ACTIVE) session_start();
 require_once "../../vendor/autoloader.php";
 $security = new Security();
 $product = new Movement();
@@ -9,14 +9,14 @@ $drawProduct = $product->drawMovement($silvia);
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="theme-color" content="#4D6E9B">
     <title>Visualizar pedido</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="icon" href="../../assets/img/image-removebg-preview.png" type="imagen/png">
     <link href="../../assets/css/style.css" rel="stylesheet">
 </head>
@@ -26,7 +26,7 @@ $drawProduct = $product->drawMovement($silvia);
         <div class="container-fluid">
             <div class="row">
                 <nav class="navbar navbar-expand-sm navbar-dark">
-                <a class="navbar-brand col-1" href="warehouse.php">
+                    <a class="navbar-brand col-1" href="warehouse.php">
                         <img src="../../assets/img/image-removebg-preview.png" alt="Avatar Logo" style="width:50px;">
                     </a>
                     <h2 class="col-10 text-center">La casa del electrodomestico</h2>
@@ -34,9 +34,12 @@ $drawProduct = $product->drawMovement($silvia);
             </div>
         </div>
     </header>
-    </div>
-        <?= $drawProduct ?>
-    </div>
+    <section>
+        <div>
+            <?= $drawProduct ?>
+        </div>
+    </section>
+
     <footer>
         <div class="container-fluid">
             <div class="row align-item-center">
@@ -48,9 +51,7 @@ $drawProduct = $product->drawMovement($silvia);
             </div>
         </div>
     </footer>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
-        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 
 </html>
