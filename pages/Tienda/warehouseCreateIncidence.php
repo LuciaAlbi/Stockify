c19e2a63093a878c8abd9fc6644c6a861514daa2
AlvaroGalvezBarba <122776088+AlvaroGalvezBarba@users.noreply.github.com>
2023-05-21 20:17:44 +0200

    Update warehouseCreateIncidence.php

--- pages/Tienda/warehouseCreateIncidence.php
+++ pages/Tienda/warehouseCreateIncidence.php
@@ -1,21 +1,20 @@
 <?php
-if(session_status() !== PHP_SESSION_ACTIVE) session_start();
+if (session_status() !== PHP_SESSION_ACTIVE) session_start();
 require_once "../../vendor/autoloader.php";
 $security = new Security();
-
-if(count($_POST)>0){
-    $incidencia = new Inci();  
-      $creatInci=[
-        'f_h'=>$_POST['fecha_hora'],
-        'desc'=>$_POST['descripcion'],
-        'a_id'=>$_POST['almacen_id'],
-        'ref'=>$_POST['ref'],
-      ];
-      $incidencia->insertInci($creatInci);
-      header('location:../Tienda/warehouseIncidence.php');
-  } else {
-      header('location:../Tienda/warehouseIncidence.php');
-  }
+if (count($_POST) > 0) {
+  $incidencia = new Inci();
+  $creatInci = [
+    'f_h' => $_POST['fecha_hora'],
+    'desc' => $_POST['descripcion'],
+    'a_id' => $_POST['almacen_id'],
+    'ref' => $_POST['ref'],
+  ];
+  $incidencia->insertInci($creatInci);
+  header('location:../Tienda/warehouseIncidence.php');
+} else {
+  header('location:../Tienda/warehouseIncidence.php');
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -26,8 +25,7 @@ if(count($_POST)>0){
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="theme-color" content="#4D6E9B">
   <title>Create Incidence</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
   <link rel="icon" href="../../assets/img/image-removebg-preview.png" type="imagen/png">
   <link href="../../assets/css/style.css" rel="stylesheet">
 </head>
@@ -73,10 +71,8 @@ if(count($_POST)>0){
     </div>
   </section>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
-    crossorigin="anonymous"></script>
-</body>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+
 <footer>
   <div class="container-fluid">
     <div class="row align-item-center">
@@ -88,5 +84,5 @@ if(count($_POST)>0){
     </div>
   </div>
 </footer>
-
+</body>
 </html>
