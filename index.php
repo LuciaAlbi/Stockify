da5665d1bd6305f4f2d6fdfb0cf3110c820ad2fd
AlvaroGalvezBarba <122776088+AlvaroGalvezBarba@users.noreply.github.com>
2023-05-21 21:17:01 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <?php
-/* ini_set('display_errors', 1);
+ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 require_once __DIR__."/vendor/autoloader.php";
 $security = new Security();
 $loginMessage = $security->doLogin();
- */
 ?>
 <!DOCTYPE html>
 
